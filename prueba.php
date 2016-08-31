89698f3a25616bad29f11ca5f0d95e9fd3ed629c
yhuleyver <yhuleyver@gmail.com>
2016-08-31 10:28:13 -0500

    se agrego una nueva linea de codigo para entender el manejo de git

--- prueba.php
+++ prueba.php
@@ -1,3 +1,5 @@
 Enviando mi primer archivo GitHub
 
 agregando una nueva linea de texto  o codigo en este archivo.
+
+realizando una nueva modificacion para que git tome los cambios y luego actualizar github.
