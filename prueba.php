615d4017b1f179165dcdf64311f9a1034a11f92d
yhuleyver <yhuleyver@gmail.com>
2016-08-31 13:33:58 -0500

    Se realizo una modificacion para ver como se trabaja con otras ramas

--- prueba.php
+++ prueba.php
@@ -3,3 +3,5 @@ Enviando mi primer archivo GitHub
 agregando una nueva linea de texto  o codigo en este archivo.
 
 realizando una nueva modificacion para que git tome los cambios y luego actualizar github.
+
+Agregando otra linea para guardar commit en  la rama test
